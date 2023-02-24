195a8f7cdc64bcc167900c61d41b97fb7941f1b5
Matjaž Prtenjak <matjaz@matjazev.net>
2023-02-24 19:17:52 +0100

    Aded few to-do items

--- csharp-basic-topics/DifferentTypesOfCommentsInCSharp/DifferentTypesOfCommentsInCSharp/Program.cs
+++ csharp-basic-topics/DifferentTypesOfCommentsInCSharp/DifferentTypesOfCommentsInCSharp/Program.cs
@@ -1,7 +1,10 @@
 ﻿using DifferentTypesOfCommentsInCSharp;
 
+// TODO: Ask a user for the number of repetitions
 for (int number = 0; number <= 15; number++)
 {
     var result = SingleLineComments.Factorial(number);
+
+    // TODO: Use colors for more excellent output
     Console.WriteLine($"Factorial of {number,3} is {result,14}");
 }
